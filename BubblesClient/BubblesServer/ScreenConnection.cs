62764f94a5bb6b1811b943d6dc7907dc4918d3b8
Pierre-André Saulais <pasaulais@free.fr>
2012-01-28 16:10:45 +0000

    Cleaned up ScreenConnection

--- BubblesClient/BubblesServer/ScreenConnection.cs
+++ BubblesClient/BubblesServer/ScreenConnection.cs
@@ -4,29 +4,17 @@ using System.Net.Sockets;
 
 namespace BubblesServer
 {
-    public delegate void ReadCallback(BubblesMessage msg);
+    /// <summary>
+    /// Function that can be used to receive messages.
+    /// </summary>
+    public delegate void MessageCallback(BubblesMessage msg);
     
-    internal class ReadOperation
-    {
-        public byte[] Buffer;
-        public ReadCallback Callback;
-        
-        public ReadOperation(int size, ReadCallback callback)
-        {
-            Buffer = new byte[size];
-            Callback = callback;
-        }
-    }
-    
+    /// <summary>
+    /// Represents a connection between a screen and the bubble server.
+    /// </summary>
     public class ScreenConnection
     {
-        private Socket m_socket;
-        private NetworkStream m_stream;
-        private StreamWriter m_writer;
-        private MemoryStream m_receiveBuffer;
-        private StreamReader m_reader;
-        
-        
+        #region Public interface
         public ScreenConnection(Socket socket)
         {
             m_socket = socket;
@@ -36,15 +24,71 @@ namespace BubblesServer
             m_reader = new StreamReader(m_receiveBuffer);
         }
         
-        public void BeginReceiveMessage(ReadCallback callback)
+        /// <summary>
+        /// Enqueue the reception of a message from the client (non blocking).
+        /// </summary>
+        /// <param name='callback'>
+        /// Function to call when a message is received.
+        /// </param>
+        public void BeginReceiveMessage(MessageCallback callback)
         {
-            ReadOperation op = new ReadOperation(256, callback);
+            ReceiveOperation op = new ReceiveOperation(256, callback);
             m_stream.BeginRead(op.Buffer, 0, op.Buffer.Length, ReadFinished, op);
         }
         
+        /// <summary>
+        /// Send a message to the client (blocking).
+        /// </summary>
+        public void SendMessage(BubblesMessage message)
+        {
+            string line = null;
+            switch(message.Type)
+            {
+            case BubblesMessageType.Add:
+                AddMessage am = (AddMessage)message;
+                line = string.Format("add {0}", am.BubbleID);
+                break;
+            default:
+                throw new Exception("Unknown message type");
+            }
+            Console.WriteLine("Sending: {0}", line);
+            m_writer.WriteLine(line);
+        }
+        #endregion
+        #region Implementation
+        private Socket m_socket;
+        private NetworkStream m_stream;
+        private StreamWriter m_writer;
+        private MemoryStream m_receiveBuffer;
+        private StreamReader m_reader;
+        
+        /// <summary>
+        /// Holds state for an asynchronous operation to receive a message.
+        /// </summary>
+        internal class ReceiveOperation
+        {
+            /// <summary>
+            /// Buffer to write data received from the socket.
+            /// </summary>
+            public byte[] Buffer;
+            /// <summary>
+            /// Function to call when a complete message has been received.
+            /// </summary>
+            public MessageCallback Callback;
+            
+            public ReceiveOperation(int size, MessageCallback callback)
+            {
+                Buffer = new byte[size];
+                Callback = callback;
+            }
+        }
+        
+        /// <summary>
+        /// Called when the asynchronous receive operation finishes.
+        /// </summary>
         private void ReadFinished(IAsyncResult result)
         {
-            ReadOperation op = (ReadOperation)result.AsyncState;
+            ReceiveOperation op = (ReceiveOperation)result.AsyncState;
             int bytesRead = m_stream.EndRead(result);
             if(bytesRead == 0)
             {
@@ -66,8 +110,15 @@ namespace BubblesServer
             }
         }
         
+        /// <summary>
+        /// Tries to read a message from the current buffered data.
+        /// </summary>
+        /// <returns>
+        /// Message read or null if there is not enough data for a complete message.
+        /// </returns>
         private BubblesMessage TryReadMessage()
         {
+            // Detect the first newline in the buffered data.
             int c;
             bool lineFound = false;
             m_receiveBuffer.Seek(0, SeekOrigin.Begin);
@@ -86,15 +137,22 @@ namespace BubblesServer
                 return null;
             }
             
+            // Read the first line
             string line;
             m_receiveBuffer.Seek(0, SeekOrigin.Begin);
             line = m_reader.ReadLine();
             
+            // Remove that line from the reception buffer
             long bytesLeft = m_receiveBuffer.Length - m_receiveBuffer.Position;
             byte[] left = new byte[bytesLeft];
             m_receiveBuffer.Read(left, 0, (int)bytesLeft);
             m_receiveBuffer = new MemoryStream(left);
             
+            return ParseMessage(line);
+        }
+        
+        private BubblesMessage ParseMessage(string line)
+        {
             string[] parts = line.Split(' ');
             if(parts.Length < 1)
             {
@@ -103,49 +161,33 @@ namespace BubblesServer
             switch(parts[0])
             {
             case "change-screen":
-                return ReadChangeScreenMessage(parts);
+                return ParseChangeScreenMessage(parts);
             default:
                 throw new Exception("Unknown message");
             }
         }
         
-        private ChangeScreenMessage ReadChangeScreenMessage(string[] parts)
+        private ChangeScreenMessage ParseChangeScreenMessage(string[] parts)
         {
             if(parts.Length != 3)
-                {
-                    throw new Exception("Invalid message: missing bubble ID or direction");
-                }
-                int bubbleID = Int32.Parse(parts[1]);
-                ScreenDirection direction = ScreenDirection.Unknown;
-                switch(parts[2])
-                {
-                case "left":
-                    direction = ScreenDirection.Left;
-                    break;
-                case "right":
-                    direction = ScreenDirection.Right;
-                    break;
-                default:
-                    throw new Exception("Invalid direction: " + parts[2]);
-                }
-                return new ChangeScreenMessage(bubbleID, direction);
-        }
-        
-        public void SendMessage(BubblesMessage message)
-        {
-            string line = null;
-            switch(message.Type)
             {
-            case BubblesMessageType.Add:
-                AddMessage am = (AddMessage)message;
-                line = string.Format("add {0}", am.BubbleID);
+                throw new Exception("Invalid message: missing bubble ID or direction");
+            }
+            int bubbleID = Int32.Parse(parts[1]);
+            ScreenDirection direction = ScreenDirection.Unknown;
+            switch(parts[2])
+            {
+            case "left":
+                direction = ScreenDirection.Left;
+                break;
+            case "right":
+                direction = ScreenDirection.Right;
                 break;
             default:
-                throw new Exception("Unknown message type");
+                throw new Exception("Invalid direction: " + parts[2]);
             }
-            Console.WriteLine("Sending: {0}", line);
-            m_writer.WriteLine(line);
+            return new ChangeScreenMessage(bubbleID, direction);
         }
+        #endregion
     }
 }
-
