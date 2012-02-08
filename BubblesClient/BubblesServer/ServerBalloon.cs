d92340a2d6d9910006a20b3b7b45c8464443f717
Pierre-Andre Saulais <pasaulais@free.fr>
2012-02-08 13:16:10 +0000

    Newly-created balloons have sane initial velocity now Always remove a balloon from the physics world when off-screen Use both X and Y components of initial velocity

--- BubblesClient/BubblesServer/ServerBalloon.cs
+++ BubblesClient/BubblesServer/ServerBalloon.cs
@@ -5,8 +5,8 @@ namespace Balloons.Server
 {
     public class ServerBalloon : Balloon
     {
-        public static readonly Vector2D VelocityLeft = new Vector2D(-0.1f, 0.0f);
-        public static readonly Vector2D VelocityRight = new Vector2D(0.1f, 0.0f);
+        public static readonly Vector2D VelocityLeft = new Vector2D(-10.0f, 0.0f);
+        public static readonly Vector2D VelocityRight = new Vector2D(10.0f, 0.0f);
 
         private Screen m_screen;
 
