aef147625adb5d514145cfb56c1100c55076bf12
wjr3 <wjr3@hw.ac.uk>
2012-02-12 15:05:18 +0000

    Added 'clap' gesture to mouse input (hold right mouse to show second hand, left click to clap)

--- BubblesClient/BubblesClient/Input/Controllers/Mouse/MouseInput.cs
+++ BubblesClient/BubblesClient/Input/Controllers/Mouse/MouseInput.cs
@@ -14,6 +14,9 @@ namespace BubblesClient.Input.Controllers.Mouse
     public class MouseInput : IInputController
     {
         private Hand _hand = new Hand();
+        private Hand _altHand = new Hand();
+        private float Seperation = 0, MaxSeperation = 200;
+        private bool anim = false;
 
         /// <summary>
         /// Does nothing; Mouse input is handled by XNA for us
@@ -29,9 +32,30 @@ namespace BubblesClient.Input.Controllers.Mouse
         {
             MouseState ms = XnaMouse.GetState();
 
-            _hand.Position = new Vector3(ms.X, ms.Y, 0);
-
-            return new Hand[] { _hand };
+            if (ms.RightButton == ButtonState.Pressed)
+            {
+                if (ms.LeftButton == ButtonState.Pressed)
+                {
+                    anim = true;
+                }
+                if (anim)
+                {
+                    Seperation = Seperation - 20;
+                }
+                _hand.Position = new Vector3(ms.X - Seperation, ms.Y, 0);
+                _altHand.Position = new Vector3(ms.X + Seperation, ms.Y, 0);
+                if (Seperation <= 0)
+                {
+                    Seperation = MaxSeperation;
+                    anim = false;
+                }
+                return new Hand[] { _hand, _altHand };
+            }
+            else
+            {
+                _hand.Position = new Vector3(ms.X, ms.Y, 0);
+                return new Hand[] { _hand };
+            }
         }
     }
 }
