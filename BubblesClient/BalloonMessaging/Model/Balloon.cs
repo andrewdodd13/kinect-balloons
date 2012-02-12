b518607fb5218d3228d1117b2423375c89ea669e
Andrew Dodd <andrewdodd13@gmail.com>
2012-02-12 17:31:43 +0000

    Update Balloon class to have new type enum.
    Changes from base:
    - Fix incorrect use of properties and get rid of public variables (guys, really?)
    
    Changes from merge:
    - Convertion methods should be static

--- BubblesClient/BalloonMessaging/Model/Balloon.cs
+++ BubblesClient/BalloonMessaging/Model/Balloon.cs
@@ -12,54 +12,94 @@ namespace Balloons.Messaging.Model
         Right
     }
 
+    public enum BalloonType
+    {
+        Customizable,
+        Twitter,
+        News,
+        CustomContent
+    }
+
     public class Balloon
     {
-        private int m_id;
-        public string Label;
-        public string Content;
-        public string Url;
+        public int ID { get; private set; }
 
-        public int Type;
-        public int OverlayType;
-        public Colour BackgroundColor;
+        public string Label { get; set; }
+        public string Content { get; set; }
+        public string Url { get; set; }
 
-        public int ID
-        {
-            get { return this.m_id; }
-        }
+        public BalloonType Type { get; set; }
+        public int OverlayType { get; set; }
+        public Colour BackgroundColor { get; set; }
 
         public Balloon(int id)
         {
-            m_id = id;
+            this.ID = id;
+
+            BackgroundColor = new Colour(255, 255, 255, 255);
         }
 
         public static string FormatDirection(Direction direction)
         {
-            switch(direction)
+            switch (direction)
             {
-            case Direction.Left:
-                return "left";
-            case Direction.Right:
-                return "right";
-            case Direction.Any:
-                return "any";
-            default:
-                throw new ArgumentOutOfRangeException();
+                case Direction.Left:
+                    return "left";
+                case Direction.Right:
+                    return "right";
+                case Direction.Any:
+                    return "any";
+                default:
+                    throw new ArgumentOutOfRangeException();
             }
         }
 
         public static Direction ParseDirection(string text)
         {
-            switch(text)
+            switch (text)
             {
-            case "left":
-                return Direction.Left;
-            case "right":
-                return Direction.Right;
-            case "any":
-                return Direction.Any;
-            default:
-                throw new ArgumentOutOfRangeException();
+                case "left":
+                    return Direction.Left;
+                case "right":
+                    return Direction.Right;
+                case "any":
+                    return Direction.Any;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        public static string FormatBalloonType(BalloonType btype)
+        {
+            switch (btype)
+            {
+                case BalloonType.CustomContent:
+                    return "customcontent";
+                case BalloonType.Customizable:
+                    return "customizable";
+                case BalloonType.News:
+                    return "news";
+                case BalloonType.Twitter:
+                    return "twitter";
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        public static BalloonType ParseBalloonType(string text)
+        {
+            switch (text)
+            {
+                case "customcontent":
+                    return BalloonType.CustomContent;
+                case "customizable":
+                    return BalloonType.Customizable;
+                case "news":
+                    return BalloonType.News;
+                case "twitter":
+                    return BalloonType.Twitter;
+                default:
+                    throw new ArgumentOutOfRangeException();
             }
         }
     }
