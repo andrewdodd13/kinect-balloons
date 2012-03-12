3e3d4b369ae26d68fcc16c5a6efabc596e4a7ed6
Brieuc Roblin <brieuc.roblin@gmail.com>
2012-03-12 15:01:58 +0000

    Added Plane messages

---
+++ BubblesClient/BalloonMessaging/Model/Plane.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Balloons.Messaging.Model
+{
+    public enum PlaneType
+    {
+        BurstBallons,
+        PushBallons
+    };
+
+    public class Plane
+    {
+        public string ID { get; private set; }
+        public float Time { get; private set; }
+        public PlaneType Type { get; private set; }
+
+        public Plane(string id, PlaneType type, float time = 0)
+        {
+            this.ID = id;
+            this.Type = type;
+            this.Time = time;
+        }
+
+        public Plane(Plane parent)
+            : this(parent.ID, parent.Type, parent.Time)
+        {
+        }
+    }
+}
---
+++ BubblesClient/BubblesServer/ServerPlane.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Balloons.Messaging.Model;
+
+namespace Balloons.Server
+{
+    class ServerPlane : Plane
+    {
+        private Screen m_screen;
+
+        public Screen Screen
+        {
+            get { return this.m_screen; }
+            set { m_screen = value; }
+        }
+
+        public ServerPlane(string planeID, PlaneType type)
+            : base(planeID, type)
+        {
+        }
+    }
+}
